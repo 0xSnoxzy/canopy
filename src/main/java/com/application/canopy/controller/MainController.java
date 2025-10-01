be94d96de0cb86124daf7f6d109bae526aa055fc
0xSnoxzy <blacksniper006@gmail.com>
2025-10-01 19:03:55 +0200

    update project structure 0.3

--- src/main/java/com/application/canopy/controller/MainController.java
+++ src/main/java/com/application/canopy/controller/MainController.java
@@ -9,6 +9,6 @@ public class MainController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("DAVID!!!!");
+        welcomeText.setText("ANTONIO!!!!");
     }
 }
