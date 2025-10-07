7cfda1c4ad5beb6f1507db901873c7e82c45204e
EsBru <davidbrunosp@gmail.com>
2025-10-07 15:55:56 +0200

    test david menu izquierdo "struttuaa"

--- src/main/java/com/application/canopy/Main.java
+++ src/main/java/com/application/canopy/Main.java
@@ -6,12 +6,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class Main extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("principale.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        URL css = getClass().getResource("/styles/theme.css");
+        if (css == null) throw new IllegalStateException("CSS no encontrado");
+        scene.getStylesheets().add(css.toExternalForm());
+
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/application/canopy/controller/MainController.java
+++ src/main/java/com/application/canopy/controller/MainController.java
@@ -1,11 +1,19 @@
 package com.application.canopy.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MainController {
+    @FXML private Label welcomeText;
+    @FXML private Button miBoton;
+
     @FXML
-    private Label welcomeText;
+    private void Prueba(ActionEvent e) {
+        Button b = (Button) e.getSource();
+        b.setText(b.getText().equals("Mostrar") ? "Ocultar" : "Mostrar");
+    }
 
     @FXML
     protected void onHelloButtonClick() {
