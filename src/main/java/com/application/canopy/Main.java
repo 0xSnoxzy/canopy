c3a13edd85cac935421896a75529f0f2a1eb42dd
EsBru <davidbrunosp@gmail.com>
2025-10-09 15:48:31 +0200

    CReazione della base di Main.java, home .fxml e nav.fxml con i bottoni di prova in nav

--- src/main/java/com/application/canopy/Main.java
+++ src/main/java/com/application/canopy/Main.java
@@ -1,4 +1,33 @@
 package com.application.canopy;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/application/canopy/view/home.fxml")));
+        Scene scene = new Scene(root, 320, 240);
+
+        URL css = getClass().getResource("/css/base.css");
+
+        if (css == null) throw new IllegalStateException("File CSS non trovato.");
+        scene.getStylesheets().add(css.toExternalForm());
+
+        stage.setScene(scene);
+        stage.setTitle("Cannopy");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
