786c49517dc52c981c5451885aa65264d66fd85c
snoxzy <104795368+0xSnoxzy@users.noreply.github.com>
2025-10-16 10:09:34 +0200

    Icone + Aggiustamenti calendar.fxml

--- src/main/java/com/application/canopy/Main.java
+++ src/main/java/com/application/canopy/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        Font.loadFont(getClass().getResourceAsStream("src/main/resources/css/fonts/AtkinsonHyperlegible-Regular.ttf"), 14);
+
+
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/application/canopy/view/calendar.fxml")));
         Scene scene = new Scene(root, 320, 240);
 
--- src/main/java/com/application/canopy/controller/NavController.java
+++ src/main/java/com/application/canopy/controller/NavController.java
@@ -1,4 +1,40 @@
 package com.application.canopy.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
+
 public class NavController {
+
+    // Profile coins
+    @FXML private ImageView iconCoins;
+
+    // Menu icons
+    @FXML private ImageView iconHome;
+    @FXML private ImageView iconAchievements;
+    @FXML private ImageView iconHerbarium;
+    @FXML private ImageView iconCalendar;
+
+    // Footer icon
+    @FXML private ImageView iconLogout;
+
+    @FXML
+    public void initialize() {
+        // Carica le immagini dal **classpath** (NON file system)
+        iconCoins.setImage(load("/com/application/canopy/view/components/images/coins.png"));
+        iconHome.setImage(load("/com/application/canopy/view/components/images/home.png"));
+        iconAchievements.setImage(load("/com/application/canopy/view/components/images/achievements.png"));
+        iconHerbarium.setImage(load("/com/application/canopy/view/components/images/herbarium.png"));
+        iconCalendar.setImage(load("/com/application/canopy/view/components/images/calendar.png"));
+        iconLogout.setImage(load("/images/icons/logout.png"));
+    }
+
+    private Image load(String classpathAbsolutePath) {
+        return new Image(Objects.requireNonNull(
+                getClass().getResourceAsStream(classpathAbsolutePath),
+                "Immagine non trovata nel classpath: " + classpathAbsolutePath
+        ));
+    }
 }
