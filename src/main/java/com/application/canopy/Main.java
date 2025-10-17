40ae4997617d476a9510d2f99b7f3c948567d5a8
snoxzy <104795368+0xSnoxzy@users.noreply.github.com>
2025-10-17 11:19:24 +0200

    icona app (di nuovo)

--- src/main/java/com/application/canopy/Main.java
+++ src/main/java/com/application/canopy/Main.java
@@ -15,11 +15,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        Font.loadFont(getClass().getResourceAsStream("src/main/resources/css/fonts/AtkinsonHyperlegible-Regular.ttf"), 14);
+        Font.loadFont(getClass().getResourceAsStream("css/fonts/AtkinsonHyperlegible-Regular.ttf"), 14);
 
+        stage.getIcons().add(
+                new javafx.scene.image.Image(
+                        Objects.requireNonNull(getClass().getResourceAsStream("/com/application/canopy/view/components/images/app/canopy.png"))
+                )
+        );
 
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/application/canopy/view/home.fxml")));
-        Scene scene = new Scene(root, 1080, 720);
+        Scene scene = new Scene(root, 1080, 620);
 
         URL css = getClass().getResource("/css/base.css");
 
